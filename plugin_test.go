0be7189df673a2ed7f0a7b2263e6da7e44c3c5ef
appleboy <appleboy.tw@gmail.com>
2024-10-20 18:42:08 +0800

    refactor: remove obsolete test functions and logic
    - Remove the `TestJSONBodyParseError` function and its associated test logic
    - Remove the `TestGitlabHost` function and its associated test logic
    
    Signed-off-by: appleboy <appleboy.tw@gmail.com>

--- plugin_test.go
+++ plugin_test.go
@@ -13,29 +13,3 @@ func TestMissingConfig(t *testing.T) {
 
 	assert.NotNil(t, err)
 }
-
-func TestJSONBodyParseError(t *testing.T) {
-	plugin := Plugin{
-		Host:  "http://example.com",
-		Token: "foo",
-		ID:    "bar",
-		Ref:   "master",
-	}
-
-	err := plugin.Exec()
-
-	assert.NotNil(t, err)
-}
-
-func TestGitlabHost(t *testing.T) {
-	plugin := Plugin{
-		Host:  "https://gitlab.com",
-		Token: "foo",
-		ID:    "bar",
-		Ref:   "master",
-	}
-
-	err := plugin.Exec()
-
-	assert.Nil(t, err)
-}
